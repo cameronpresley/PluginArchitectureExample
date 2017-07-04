3c52b970f3f078a522c61f2bec76379fbc655199
Cameron Presley <Cameron@TheSoftwareMentor.com>
2017-07-04 07:22:44 -0400

    Added MessageRetrieve class.

---
+++ Plugin.Core/MessageRetriever.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.Core
+{
+    public abstract class MessageRetriever
+    {
+        public abstract IEnumerable<Message> RetrieveMessages();
+    }
+}

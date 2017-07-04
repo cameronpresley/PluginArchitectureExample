686d2db4eef485769c32ffb83fa199b8295f5909
Cameron Presley <Cameron@TheSoftwareMentor.com>
2017-07-04 09:42:19 -0400

    Added EmailMessageWorkflow class.

---
+++ EmailPlugin/EmailMessageWorkflow.cs
@@ -0,0 +1,13 @@
+﻿using Plugin.Core;
+
+namespace EmailPlugin
+{
+    public class EmailMessageWorkflow : Workflow
+    {
+        public EmailMessageWorkflow(EmailMessageRetriever retriever, EmailMessageSender sender) : base(retriever, sender)
+        {
+        }
+
+        public override string Name => "Email Sender";
+    }
+}

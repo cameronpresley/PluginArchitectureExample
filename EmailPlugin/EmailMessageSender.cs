2f38e8f161f1621d2f6b3f0f2ecc3b5d68c3fd71
Cameron Presley <Cameron@TheSoftwareMentor.com>
2017-07-04 09:40:47 -0400

    Renamed parameter of MessageSender to be messages, not message. Added EmailMessageSender.

---
+++ EmailPlugin/EmailMessageSender.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using Plugin.Core;
+
+namespace EmailPlugin
+{
+    public class EmailMessageSender : MessageSender
+    {
+        public override void SendMessage(List<Message> messages)
+        {
+            foreach (var message in messages.OfType<EmailMessage>())
+            {
+                using (var smtp = new SmtpClient("smtp.google.com"))
+                {
+                    var mailMessage = new MailMessage(message.From, message.To, message.Subject, message.Message);
+                    smtp.Send(mailMessage);
+                }
+            }
+        }
+    }
+}

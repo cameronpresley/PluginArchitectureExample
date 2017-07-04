f72374f1e92182c510c700565223709473f108db
Cameron Presley <Cameron@TheSoftwareMentor.com>
2017-07-04 07:17:12 -0400

    Added solution and winforms project.

---
+++ Plugin.Core/MessageSender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.Core
+{
+    public interface MessageSender
+    {
+        void SendMessage(Message message);
+    }
+}

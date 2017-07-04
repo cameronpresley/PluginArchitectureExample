e973bdf169494e22481cc65bbfda9fa5f3ab8ed0
Cameron Presley <Cameron@TheSoftwareMentor.com>
2017-07-04 07:48:40 -0400

    Added generic Workflow class to Core.

---
+++ Plugin.Core/Workflow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.Core
+{
+    public class Workflow
+    {
+        public void Do(MessageRetriever retriever, MessageSender sender)
+        {
+            try
+            {
+                retriever.RetrieveMessages().ToList().ForEach(sender.SendMessage);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to retrieve or send messages.");
+            }
+        }
+    }
+}

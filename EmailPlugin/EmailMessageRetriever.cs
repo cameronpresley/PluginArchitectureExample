422e6f94f074be95297f434309b9995bb8aec291
Cameron Presley <Cameron@TheSoftwareMentor.com>
2017-07-04 09:36:53 -0400

    Added the EmailMessageRetriever.

---
+++ EmailPlugin/EmailMessageRetriever.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Plugin.Core;
+
+namespace EmailPlugin
+{
+    public class EmailMessageRetriever : MessageRetriever
+    {
+        public override IEnumerable<Message> RetrieveMessages()
+        {
+            return new List<EmailMessage>
+            {
+                new EmailMessage{From = "Company", To = "Employee", Subject = "Congratulations", Message = "Good job, here's some extra money."},
+                new EmailMessage{From = "Company2", To = "Company", Subject = "Cutbacks", Message = "Spending way too much money..."}
+            };
+        }
+    }
+}
